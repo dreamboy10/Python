58a80c9ffc7781c5fd5558e417636ca0c1258737
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-01-20 02:18:22 +0900

    2022.01.20

---
+++ test15.py
@@ -0,0 +1,25 @@
+str_input = input("태어난 해를 입력해 주세요> ")
+birth_year = int(str_input) % 12
+
+if birth_year == 0:
+	print("원숭이 띠 입니다.")
+elif birth_year == 1:
+	print("닭 띠 입니다.")
+elif birth_year == 2:
+	print("개 띠 입니다.")
+elif birth_year == 3:
+	print("돼지 띠 입니다.")
+elif birth_year == 4:
+	print("쥐 띠 입니다.")
+elif birth_year == 5:
+	print("소 띠 입니다.")
+elif birth_year == 6:
+	print("범 띠 입니다.")
+elif birth_year == 7:
+	print("토끼 띠 입니다.")
+elif birth_year == 9:
+	print("뱀 띠 입니다.")
+elif birth_year == 10:
+	print("말 띠 입니다.")
+elif birth_year == 11:
+	print("양 띠 입니다.")
